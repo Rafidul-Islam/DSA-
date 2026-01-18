09514e397ed7bfff0ded08ddf431970bd5297b6b
Rafid <rafid.iconic@gmail.com>
2026-01-18 11:34:15 +0600

    refractor file structure & create first program in java (not first X)

---
+++ src/com/rafid/dsa/day01/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.rafid.dsa.day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World ! ");
+        System.out.println("This is day 01 learning java");
+    }
+}
